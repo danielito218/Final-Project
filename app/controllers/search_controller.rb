d25de720fe273260093cd6b420c418efd96e8758
danielostos21 <danielostos21@gmail.com>
2022-05-04 00:51:35 -0400

    Add a search by timelapse for amount generated

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,25 @@
 class SearchController < ApplicationController
   def create
+    if params[:initial_date] == ""
+      initial_date = Date.new(1,1,1)
+    else
+      initial_date = params[:initial_date].to_date
+    end
+
+    if params[:final_date] == ""
+      final_date = Date.today
+    else
+      final_date = params[:final_date].to_date
+    end
+
+    @results = []
+
+    ProjectStatus.all.each do |project_status|
+      transiton_date = project_status.tansition_date.to_date
+      if initial_date <= transiton_date and transiton_date <= final_date
+          @results.push(project_status)       
+      end
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/clients/:id/clientprojects', to: 'clients#client_projects', as: "clientprojects"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'home/search_by_timelapse'
   # Defines the root path route ("/")
   # root "articles#index"
 end
